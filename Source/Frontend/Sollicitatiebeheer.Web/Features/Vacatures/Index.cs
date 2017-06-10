aebb62a2ba428b17da1d14d8b244007f30229ef9
Yves Schelpe <yves.schelpe@gmail.com>
2017-06-10 12:22:36 +0200

    Added example of how to build a simple request, this with sorting and with returning the request message back into the response.

--- Source/Frontend/Sollicitatiebeheer.Web/Features/Vacatures/Index.cs
+++ Source/Frontend/Sollicitatiebeheer.Web/Features/Vacatures/Index.cs
@@ -1,22 +1,47 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using MediatR;
 
 namespace Sollicitatiebeheer.Web.Features.Vacatures {
     public class Index {
         public class Handler : IRequestHandler<Request, Response> {
             public Response Handle(Request message) {
-                return new Response(new List<string> {
+                var vacatures = new List<string> {
                     "Vacature 1",
                     "Vacature 2",
                     "Vacature 3"
-                });
+                };
+
+                if (String.IsNullOrEmpty(message.SortOrder)) {
+                    message.SortOrder = "asc";
+                }
+
+                switch (message.SortOrder.ToLower().Trim()) {
+                    case "asc":
+                        vacatures = vacatures.OrderBy(v => v).ToList();
+                        break;
+                    case "desc":
+                        vacatures = vacatures.OrderByDescending(v => v).ToList();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(message.SortOrder), $"Sort order {message.SortOrder} does not exist.");
+                }
+
+                return new Response(message, vacatures);
             }
         }
 
-        public class Request : IRequest<Response> { }
+        public class Request : IRequest<Response> {
+            public Request() { }
+            public Request(Request request) {
+                SortOrder = request.SortOrder;
+            }
+            public string SortOrder { get; set; }
+        }
 
-        public class Response {
-            public Response(List<string> vacatures) => Vacatures = vacatures;
+        public class Response : Request {
+            public Response(Request request, List<string> vacatures) : base(request) => Vacatures = vacatures;
             public List<string> Vacatures { get; }
         }
     }

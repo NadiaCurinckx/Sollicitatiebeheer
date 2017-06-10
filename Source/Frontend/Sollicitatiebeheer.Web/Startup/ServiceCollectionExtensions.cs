06c1337c4a50bf9462caf8c290401aaf52261d10
Yves Schelpe <yves.schelpe@gmail.com>
2017-06-10 11:12:54 +0200

    Configured Autofac and MediatR

---
+++ Source/Frontend/Sollicitatiebeheer.Web/Startup/ServiceCollectionExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyModel;
+
+namespace Sollicitatiebeheer.Web.Startup {
+    /// <summary>
+    /// Extension methods for <see cref="IServiceCollection"/> to make Startup.cs readible.
+    /// </summary>
+    public static class ServiceCollectionExtensions {
+        /// <summary>
+        /// Adds Autofac as DI container, configures it and returns the IServiceProvider.
+        /// </summary>        
+        public static IServiceProvider AddAndConfigureAutofac(this IServiceCollection serviceCollection) {
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.Populate(serviceCollection);
+
+            var container = containerBuilder.Build();
+            var serviceProvider = new AutofacServiceProvider(container);
+
+            return serviceProvider;
+        }
+
+        /// <summary>
+        /// Adds MediatR and scans all referenced assemblies for types used.
+        /// </summary>        
+        public static void AddAndConfigureMediatR(this IServiceCollection serviceCollection) {
+            serviceCollection.AddMediatR(GetReferencedAssemblies());
+        }
+
+        private static Assembly[] GetReferencedAssemblies() {
+            var assemblies = new List<Assembly>();
+            var dependencies = DependencyContext.Default.RuntimeLibraries;
+
+            foreach (var library in dependencies) {
+                if (IsCandidateCompilationLibrary(library)) {
+                    var assembly = Assembly.Load(new AssemblyName(library.Name));
+                    assemblies.Add(assembly);
+                }
+            }
+
+            assemblies.Add(typeof(Startup).GetTypeInfo().Assembly);
+
+            return assemblies.ToArray();
+        }
+
+        private static bool IsCandidateCompilationLibrary(RuntimeLibrary library) {
+            return library.Name == nameof(Sollicitatiebeheer)
+                   || library.Dependencies.Any(d => d.Name.StartsWith(nameof(Sollicitatiebeheer)));
+        }
+    }
+}
--- Source/Frontend/Sollicitatiebeheer.Web/Startup/Startup.cs
+++ Source/Frontend/Sollicitatiebeheer.Web/Startup/Startup.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Reflection;
+using Autofac.Extensions.DependencyInjection;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,10 +22,16 @@ namespace Sollicitatiebeheer.Web.Startup {
         public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
+        public IServiceProvider ConfigureServices(IServiceCollection services) {
             // Add framework services.
             services.AddMvc()
                     .AddFeatureFolders();
+
+            // Add application services.
+            services.AddAndConfigureMediatR();
+
+            // Configure autofac DI and return the service provider to ASP.NET Core
+            return services.AddAndConfigureAutofac();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

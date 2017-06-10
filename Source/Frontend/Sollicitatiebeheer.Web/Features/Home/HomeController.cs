e4e381f0bcc3785100a74749c5c24de272cd8a93
Yves Schelpe <yves.schelpe@gmail.com>
2017-06-10 10:08:11 +0200

    Added initial web project with feature folders.

---
+++ Source/Frontend/Sollicitatiebeheer.Web/Features/Home/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sollicitatiebeheer.Web.Features.Home {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
---
+++ Source/Frontend/Sollicitatiebeheer.Web/Startup/ApplicationBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Sollicitatiebeheer.Web.Startup {
+    /// <summary>
+    /// Extension methods for <see cref="IApplicationBuilder"/> to make Startup.cs readible.
+    /// </summary>
+    public static class ApplicationBuilderExtensions {
+        /// <summary>
+        /// Configures the pipeline to use Mvc with a default routing in place.
+        /// </summary>
+        /// <param name="applicationBuilder">Pipeline to add configuration to.</param>
+        public static IApplicationBuilder UseMvcWithDefaultRoutes(this IApplicationBuilder applicationBuilder) {
+            return applicationBuilder.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}

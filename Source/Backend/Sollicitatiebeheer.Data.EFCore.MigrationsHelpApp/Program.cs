952c2a82cc87bf950b2c64c6901c6cf01c9940f7
Yves Schelpe <yves.schelpe@gmail.com>
2017-06-11 12:24:50 +0200

    Added SeedDatabase batch file

--- Source/Backend/Sollicitatiebeheer.Data.EFCore.MigrationsHelpApp/Program.cs
+++ Source/Backend/Sollicitatiebeheer.Data.EFCore.MigrationsHelpApp/Program.cs
@@ -1,9 +1,42 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Sollicitatiebeheer.Model.Vacatures;
 
 namespace Sollicitatiebeheer.Data.EFCore.MigrationsHelpApp {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("EFCore Migrations Helper");
+
+            if (args == null || args.Length == 0)
+                return;
+
+            if (args[0].Equals("seed")) {
+                var connectionString =
+                    Environment.GetEnvironmentVariable("SOLLICITATIEBEHEER_ConnectionStrings:Sollicitatiebeheer");
+                var dbContextOptions = new DbContextOptionsBuilder<SollicitatiebeheerContext>();
+                dbContextOptions.UseSqlServer(connectionString);
+                var options = dbContextOptions.Options;
+                using (var db = new SollicitatiebeheerContext(options)) {
+
+                    if (!db.Vacatures.Any()) {
+                        CreateVacatures(db);
+                    }
+                }
+
+                Console.WriteLine("Seeding done.");
+            }
+        }
+
+        private static void CreateVacatures(SollicitatiebeheerContext db) {
+            Console.WriteLine("Creating vacatures...");
+            db.Vacatures.AddRange(
+                new VacatureBuilder().WithNaam("Vacature 1").Build(),
+                new VacatureBuilder().WithNaam("Vacature 2").Build(),
+                new VacatureBuilder().WithNaam("Vacature 3").Build()
+            );
+            db.SaveChanges();
+            Console.WriteLine("Vacatures created.");
         }
     }
 }
---
+++ Source/Backend/Sollicitatiebeheer.Data.EFCore/SeedDatabase.bat
@@ -0,0 +1,3 @@
+cd ../Sollicitatiebeheer.Data.EFCore.MigrationsHelpApp 
+dotnet run seed
+cd ../Sollicitatiebeheer.Data.EFCore
